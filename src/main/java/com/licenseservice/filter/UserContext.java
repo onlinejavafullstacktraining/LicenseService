b847accc35993f596fa7898d1595cb63da01a20e
Javatechnology <javatechnology.github@gmail.com>
2022-09-19 15:58:12 +0530

    Spring boot with Microservice-LicenseService

--- src/main/java/com/licenseservice/filter/UserContext.java
+++ src/main/java/com/licenseservice/filter/UserContext.java
@@ -1,24 +1,24 @@
 package com.licenseservice.filter;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.stereotype.Component;
 
 
 @Component
 public class UserContext {
     public static final String CORRELATION_ID = "tmx-correlation-id";
-    public static final String AUTH_TOKEN     = "tmx-auth-token";
-    public static final String USER_ID        = "tmx-user-id";
+    public static final String AUTH_TOKEN = "tmx-auth-token";
+    public static final String USER_ID = "tmx-user-id";
     public static final String ORGANIZATION_ID = "tmx-organization-id";
 
-    private String correlationId= new String();
-    private String authToken= new String();
-    private String userId = new String();
-    private String organizationId = new String();
+    private String correlationId = "";
+    private String authToken = "";
+    private String userId = "";
+    private String organizationId = "";
 
-    public String getCorrelationId() { return correlationId;}
+    public String getCorrelationId() {
+        return correlationId;
+    }
+
     public void setCorrelationId(String correlationId) {
         this.correlationId = correlationId;
     }
@@ -38,9 +38,11 @@ public class UserContext {
     public void setUserId(String userId) {
         this.userId = userId;
     }
+
     public String getOrganizationId() {
         return organizationId;
     }
+
     public void setOrganizationId(String organizationId) {
         this.organizationId = organizationId;
     }
